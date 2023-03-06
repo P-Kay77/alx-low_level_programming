fc6b6e26d0d5a8722f882c5a169181459664eaef
P-Kay77 <bbaidoe75@gmail.com>
2023-03-06 09:32:23 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+even more pointerss, arrays and strings
---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+*_putchar  writes the character c to dstdout
+*@c" The character to prnt
+*
+Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _utchar(char c)
+{
+	return (write (1, &c, 1))
+}
