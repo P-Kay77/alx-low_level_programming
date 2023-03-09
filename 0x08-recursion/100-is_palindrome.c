31be49f93cc6bb3765e680ad71be119844685404
P-Kay77 <bbaidoe75@gmail.com>
2023-03-08 18:53:12 -0800

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,7 +6,7 @@ int _strlen_recursion(char *s);
  * @s: string to reverse
  * Return: 1 if it is, 0 it's not
  */
-int is_palindrome(char *s) 
+int is_palindrome(char *s)
 {
 if (*s == 0)
 return (1);
@@ -17,7 +17,7 @@ return (check_pal(s, 0, _strlen_recursion(s)));
  * @s: string to calculate the length of
  * Return: length of the string
  */
-int _strlen_recursion(char *s) 
+int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 return (0);
@@ -32,7 +32,7 @@ return (1 + _strlen_recursion(s + 1));
  */
 int check_pal(char *s, int i, int len)
 {
-if (*(s + i) != *(s + len - 1)) 
+if (*(s + i) != *(s + len - 1))
 return (0);
 if (i >= len)
 return (1);
